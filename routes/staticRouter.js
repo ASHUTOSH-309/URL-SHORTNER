8d81c5cfee95b3443f664611b07b977c04c9bc63
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-07-17 16:20:42 +0530

    render homepage with a form and analytics table

---
+++ routes/staticRouter.js
@@ -0,0 +1,17 @@
+const express=require("express")
+const URL = require("../models/url")
+
+
+const router=express.Router()
+
+router.get("/",async(req,res)=>{
+    const allURLS= await URL.find({})
+    
+    return res.render("home",{
+            urls:allURLS
+
+    })
+})
+
+
+module.exports=router
