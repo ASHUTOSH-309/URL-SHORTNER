9b5d60d7c49d23880fac1919c11ce48c60dbb8e5
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-07-18 18:29:19 +0530

    Add Comment

--- service/auth.js
+++ service/auth.js
@@ -2,21 +2,18 @@ const sessionIdToUserMap=new Map();
 
 
 function setUser(id,user){
-
     sessionIdToUserMap.set(id,user)
-
-
 }
 
 
 function getUser(id){
-
     return sessionIdToUserMap.get(id)
-
-
 }
 
 module.exports={
     setUser,
     getUser
 }
+
+//Map values does not persist after server restarts
+//TO store data persistently we require databases.
