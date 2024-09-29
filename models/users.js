9ced6e05709c9b8fb271e00fee3b308a957ba0d7
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-09-29 16:18:30 +0530

    Creating a collection on the mongodb database

--- models/users.js
+++ models/users.js
@@ -22,5 +22,6 @@ const userSchema=new mongoose.Schema({
 
 
 const User=mongoose.model('user',userSchema)
+//The first parameter signifies the model name in the mongoDb database, it will be automatically pluralized
 
 module.exports=User
